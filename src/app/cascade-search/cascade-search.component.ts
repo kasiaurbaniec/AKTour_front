4b9b84a6518b8c694a79f2c79af7b10833f26f7b
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 11:25:47 +0200

    -cascade search working

--- src/app/cascade-search/cascade-search.component.ts
+++ src/app/cascade-search/cascade-search.component.ts
@@ -3,41 +3,66 @@ import { Continent } from '../model/continent'
 import { Country } from '../model/country'
 import { Trip } from '../model/trip'
 import { CascadeSearchService } from '../services/cascade-search.service'
-import { filter } from 'rxjs/operators';
 import { TripService } from '../services/trip.service';
+
 @Component({
   selector: 'app-cascade-search',
   templateUrl: './cascade-search.component.html',
   styleUrls: ['./cascade-search.component.css']
 })
 export class CascadeSearchComponent implements OnInit {
-  selectedContinent: Continent = {continent_id: 1, name:"Europe"};
-  selectedCountry: Country=     {id: 1,name: "Poland"};
+  selectedContinent: Continent={continent_id:110, name:'Select'};
+  selectedCountry: Country={id:110, name:'Select'};
   continents: Continent[];
   countries: Country[];
   tripsFiltered: Trip[];
-  result:string;
+  hasResult:boolean;
+  continentHasCountry:boolean;
+  info: string;
   constructor(
     private cascadeSearchService: CascadeSearchService,
     private tripService: TripService) 
     { }
 
   ngOnInit() {
+    this.hasResult=true;
+    this.continentHasCountry=true;
     this.cascadeSearchService.getContinents().subscribe(data=>this.continents=data);
+    this.cascadeSearchService.getCountries().subscribe(data=>this.countries=data);
     this.tripService.findAll().subscribe(data=>this.tripsFiltered=data);
-    this.result="All Trips - Choose your filter";
     this.onSelectContinent(this.selectedContinent.name);
   }
 
   onSelectContinent(continentName){
-    this.cascadeSearchService.getCountriesByContinentName(continentName).subscribe(data=>this.countries=data);
-    this.result="Trips found for "+continentName;
-    this.cascadeSearchService.getTripsByContinentName(continentName).subscribe(data=>this.tripsFiltered=data);
-    this.onSelectCountry(this.selectedCountry.name);
+    this.getTripsByContinent(continentName);
+    this.cascadeSearchService.getCountriesByContinentName(continentName).subscribe(dataCounties=>{
+      this.continentHasCountry = dataCounties.length > 0 ? true : false;
+      if(this.continentHasCountry){
+        this.countries=dataCounties;
+        
+      } else {
+        this.hasResult=false;
+        }
+    },
+    error =>{console.log(error.error.ApiError.message);
+    });
   }
-
   onSelectCountry(countryName){
-    this.result="Trips found for "+countryName;
-    this.cascadeSearchService.getTripsByCountryName(countryName).subscribe(data=>this.tripsFiltered=data);
+    this.cascadeSearchService.getTripsByCountryName(countryName).subscribe(data=>{
+      this.hasResult=data.length> 0 ? true: false;
+      this.tripsFiltered=data;
+      console.log(event);
+    }, 
+    error=> {console.log(error.error.ApiError.message);  
+    });
+  }
+  getTripsByContinent(continentName){
+    this.cascadeSearchService.getTripsByContinentName(continentName).subscribe(data=>{
+      this.hasResult=data.length> 0 ? true: false;
+      this.tripsFiltered=data;
+      console.log(event);
+    }, 
+    error=> {console.log(error.error.ApiError.message);  
+    });
   }
 }
---
+++ src/app/model/api-error.ts
@@ -0,0 +1,8 @@
+import { Timestamp } from 'rxjs';
+
+export class ApiError {
+  status?: string;
+  message?: string;
+  timestamp?: string;
+  desctiption?: string;
+}
