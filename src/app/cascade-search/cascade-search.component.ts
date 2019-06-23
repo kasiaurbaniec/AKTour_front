bb84d9bf475a0de03a97981242ea28fcf3523fea
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-23 20:10:52 +0200

    add method onChangeCities modify methods onChangeCountry and onChangeContinent

--- src/app/cascade-search/cascade-search.component.ts
+++ src/app/cascade-search/cascade-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Continent } from '../model/continent'
 import { Country } from '../model/country'
+import { City } from '../model/city'
 import { Trip } from '../model/trip'
 import { CascadeSearchService } from '../services/cascade-search.service'
 import { TripService } from '../services/trip.service';
@@ -13,8 +14,10 @@ import { TripService } from '../services/trip.service';
 export class CascadeSearchComponent implements OnInit {
   selectedContinent: Continent={continent_id:110, name:'Select'};
   selectedCountry: Country={id:110, name:'Select'};
+  selectedCity: City={id:110, name:'Select'};
   continents: Continent[];
   countries: Country[];
+  cities:City[];
   tripsFiltered: Trip[];
   hasResult:boolean;
   continentHasCountry:boolean;
@@ -29,6 +32,7 @@ export class CascadeSearchComponent implements OnInit {
     this.continentHasCountry=true;
     this.cascadeSearchService.getContinents().subscribe(data=>this.continents=data);
     this.cascadeSearchService.getCountries().subscribe(data=>this.countries=data);
+    this.cascadeSearchService.getCities().subscribe(data=>this.cities=data);
     this.tripService.findAll().subscribe(data=>this.tripsFiltered=data);
     this.onSelectContinent(this.selectedContinent.name);
   }
@@ -39,7 +43,7 @@ export class CascadeSearchComponent implements OnInit {
       this.continentHasCountry = dataCounties.length > 0 ? true : false;
       if(this.continentHasCountry){
         this.countries=dataCounties;
-        
+        this.cascadeSearchService.getCitiesByContinentName(continentName).subscribe(dataCit=>this.cities=dataCit);
       } else {
         this.hasResult=false;
         }
@@ -47,11 +51,24 @@ export class CascadeSearchComponent implements OnInit {
     error =>{console.log(error.error.ApiError.message);
     });
   }
+
   onSelectCountry(countryName){
+    this.getTripsByCountry(countryName);
+    this.cascadeSearchService.getCitiesByCountryName(countryName).subscribe(dataCities=>{
+        this.continentHasCountry = dataCities.length > 0 ? true : false;
+        if(this.continentHasCountry){
+          this.cities=dataCities;
+        } else {
+          this.hasResult=false;
+        }
+      },
+      error =>{console.log(error.error.ApiError.message);
+      });
+  }
+  getTripsByCountry(countryName){
     this.cascadeSearchService.getTripsByCountryName(countryName).subscribe(data=>{
       this.hasResult=data.length> 0 ? true: false;
       this.tripsFiltered=data;
-      console.log(event);
     }, 
     error=> {console.log(error.error.ApiError.message);  
     });
@@ -60,9 +77,16 @@ export class CascadeSearchComponent implements OnInit {
     this.cascadeSearchService.getTripsByContinentName(continentName).subscribe(data=>{
       this.hasResult=data.length> 0 ? true: false;
       this.tripsFiltered=data;
-      console.log(event);
     }, 
     error=> {console.log(error.error.ApiError.message);  
     });
+  }
+  onSelectCity(cityName){
+    this.cascadeSearchService.getTripsByCityName(cityName).subscribe(data=>{
+        this.hasResult=data.length> 0 ? true: false;
+        this.tripsFiltered=data;
+      },
+      error=> {console.log(error.error.ApiError.message);
+      });
   }
 }
