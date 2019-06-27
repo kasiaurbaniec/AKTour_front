6be2c4c031a9e35329c8b45924eeacb707033313
Alan Turek <alanturek@gmail.com>
2019-06-27 20:30:55 +0200

    add navbar with AKTours logo add image_slider add dropdown menu

---
+++ src/app/img-slider/img-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-slider',
+  templateUrl: './img-slider.component.html',
+  styleUrls: ['./img-slider.component.css']
+})
+export class ImgSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
