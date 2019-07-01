c87485e51280ba0648bb6cfd84b38a68ca400581
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-07-01 16:52:46 +0200

    first try do custom validator

--- src/app/add-trip/add-trip.component.ts
+++ src/app/add-trip/add-trip.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AddTripService} from "../services/add-trip.service";
 import {Hotel} from "../model/hotel";
 import {Airport} from "../model/airport";
-import {FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 import {Trip} from "../model/trip";
 
 
@@ -28,11 +28,11 @@ export class AddTripComponent implements OnInit {
     this.addTripService.getHotels().subscribe(data => this.hotels = data);
     this.addTripService.getAirports().subscribe(data => this.airports = data);
     this.addTripForm = new FormGroup({
-      boardType: new FormControl(null),
+      boardType: new FormControl(null,Validators.required),
       hotel: new FormControl(null),
       departureDate: new FormControl(null),
       returnDate: new FormControl(null),
-      numberOfDays: new FormControl(null),
+      numberOfDays: new FormControl(null,this.correctValueValidator),
       homeAirport: new FormControl(null),
       destinAirport: new FormControl(null),
       adultPrice: new FormControl(null),
@@ -59,7 +59,15 @@ export class AddTripComponent implements OnInit {
     this.trip.childrenVacancy = this.addTripForm.value.childrenVacancy;
     this.addTripService.addTrip(this.trip).subscribe(data => this.trip = data);
     console.log(this.trip);
+    this.addTripForm.reset();
   }
+correctValueValidator(control: AbstractControl): ValidationErrors{
+    const numberOfDaysValue=<[number]>control.value;
+    if(numberOfDaysValue==(this.minReturnDate-this.minDepartureDate).toString()){
+      return{'wrong numberOfDays value':true};
+    }
+}
+
 
 }
 
